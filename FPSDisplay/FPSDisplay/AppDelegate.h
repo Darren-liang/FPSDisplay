c8e394876268e728dd1c20e6a16aa885774bafb9
liangweidong <liangweidong@1-89-169-192.static.reverse.lstn.net>
2019-01-24 09:33:07 +0800

    create project

---
+++ FPSDisplay/FPSDisplay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FPSDisplay
+//
+//  Created by liangweidong on 2019/1/24.
+//  Copyright © 2019 liangweidong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FPSDisplay/FPSDisplay/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  FPSDisplay
+//
+//  Created by liangweidong on 2019/1/24.
+//  Copyright © 2019 liangweidong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"实时监测帧率");
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
